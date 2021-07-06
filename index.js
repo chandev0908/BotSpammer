0d1cb84ed657f3c4c9e506440f2d3cf0e609c284
chandev0908 <chandev0908@gmail.com>
2021-07-06 17:52:02 +0800

    removed delay

--- index.js
+++ index.js
@@ -13,150 +13,148 @@ client.on("message", (msg) => {
   if (msg.channel.id === "746017057669382276") {
     msg.embeds.forEach((embed) => {
       try {
-        setTimeout(() => {
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Sayu Ogiwara"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Amatsuka Uto"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Vladilena Milizé"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Elma"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Aoi Nabi"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Laika"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Ray"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Near"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Zhongli"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Kaedehara Kazuha"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          //Romeo wish
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Nyanners"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Josee"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Sig (Gangsta)"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Kaoruko Hanayagi"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Junna Hoshimi"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          // Cevan wish
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Skull Knight"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Trevor Belmont"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Musashi Miyamoto"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Shinei Nouzen"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-          if (
-            msg.author.id == "432610292342587392" &&
-            embed.author.name === "Irene"
-          ) {
-            msg.react("❤️");
-            console.log("You get " + embed.author.name);
-          }
-        }, 200);
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Sayu Ogiwara"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Amatsuka Uto"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Vladilena Milizé"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Elma"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Aoi Nabi"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Laika"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Ray"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Near"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Zhongli"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Kaedehara Kazuha"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        //Romeo wish
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Nyanners"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Josee"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Sig (Gangsta)"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Kaoruko Hanayagi"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Junna Hoshimi"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        // Cevan wish
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Skull Knight"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Trevor Belmont"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Musashi Miyamoto"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Shinei Nouzen"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
+        if (
+          msg.author.id == "432610292342587392" &&
+          embed.author.name === "Irene"
+        ) {
+          msg.react("❤️");
+          console.log("You get " + embed.author.name);
+        }
       } catch (ex) {
         console.log("ERROR: " + ex);
       }
